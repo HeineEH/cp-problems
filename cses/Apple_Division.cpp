fe7443bebbf552a17fa0c9ea1587c7c8ab240964
HeineEH <heine.elias.husdal@gmail.com>
2023-01-29 21:26:13 +0100

    Finish Apple Division

---
+++ cses/Apple_Division.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i,a,b) for (int i=a; i<b; ++i)
+#define rev(i,a,b) for (int i=a; i>b; --i)
+
+#define ll long long
+
+#define MAXN 21
+ll apples[MAXN];
+
+vector<ll> allCombs(int idx) {
+
+    if (idx == 0) return {apples[0], -apples[0]};
+
+    vector<ll> combinations = allCombs(idx-1);
+    vector<ll> res;
+
+    for (auto a : combinations) {
+        res.push_back(a-apples[idx]);
+        res.push_back(a+apples[idx]);
+    }
+
+    return res;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    rep(i, 0, n) cin >> apples[i];
+
+    vector<ll> combs = allCombs(n-1);
+    for (auto &comb : combs) comb = abs(comb);
+    ll minComb = *min_element(combs.begin(), combs.end());
+    cout << minComb;
+}
