c22af304ee55e3332a5b5094ae94347468f19043
HeineEH <heine.elias.husdal@gmail.com>
2023-01-29 10:11:48 +0100

    Finish Shortest Routes I

---
+++ cses/Shortest_Routes_I.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+#define MAXN (int)1e5
+#define ll long long
+ 
+priority_queue<pair<ll, ll>, vector<pair<ll, ll>>, greater<pair<ll, ll>>> pq;
+bool visited[MAXN];
+long long distanceFromStart[MAXN];
+vector<pair<ll, ll>> adjList[MAXN];
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    for (int i=0; i<m; i++) {
+        ll a, b, w;
+        cin >> a >> b >> w;
+ 
+        a--; b--;
+ 
+        adjList[a].push_back({w, b});
+    }
+ 
+
+    long long distanceFromStart[n];
+    for (int i=0; i<n; i++) distanceFromStart[i] = LLONG_MAX;
+
+    distanceFromStart[0] = 0;
+    pq.push({0, 0});
+ 
+    
+ 
+ 
+    ll currWeight;
+    int currNode;
+ 
+    while (!pq.empty()) {
+        tie(currWeight, currNode) = pq.top(); pq.pop();        
+        
+        if (visited[currNode]) {
+            continue;
+        }
+ 
+        for (auto &adj : adjList[currNode]) {
+            ll newWeight = currWeight+adj.first;
+            if (newWeight < distanceFromStart[adj.second]) {
+                distanceFromStart[adj.second] = newWeight;
+                pq.push({newWeight, adj.second});
+            }
+        }
+        visited[currNode] = true;
+    }
+ 
+ 
+    for (auto &a : distanceFromStart) {
+        cout << a << ' ';
+    }
+}
